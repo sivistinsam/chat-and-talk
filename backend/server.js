bb044080565577d7bf528cc48d400457e8e8f17b
Sandeep Kumar Gupta <57314972+sivistinsam@users.noreply.github.com>
2023-08-05 00:37:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,18 +24,18 @@ app.use("/api/message", messageRoutes);
 
 // deployment code -------------------------------------------------
 
- const __dirname1 = path.resolve();
- if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname1,'/frontend/build')));
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
-  });
- }
- else{
-  app.get('/',(req,res)=>{
-    res.send("API is running successfully");
-  })
- }
+ //const __dirname1 = path.resolve();
+ //if(process.env.NODE_ENV === 'production'){
+  //app.use(express.static(path.join(__dirname1,'/frontend/build')));
+  //app.get("*",(req,res)=>{
+    //res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+  //});
+ //}
+ //else{
+  //app.get('/',(req,res)=>{
+    //res.send("API is running successfully");
+  //})
+ //}
 
 // deploypment code end here -------------------------------------------------
 app.use(notFound);
