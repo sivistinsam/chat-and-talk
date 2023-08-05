19c30235d2cdb25327df0cde9d9bc5d045557d1d
Sandeep Kumar Gupta <57314972+sivistinsam@users.noreply.github.com>
2023-08-05 15:36:17 +0530

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,7 +1,9 @@
+// Importing required modules and dependencies.
 const asyncHandler = require("express-async-handler");
 const User = require("../Models/userModel");
 const generateToken = require("../config/generateToken");
 
+// Function to fetch all users with optional search query.
 const allUsers = asyncHandler(async (req, res) => {
   const keyword = req.query.search
     ? {
@@ -12,12 +14,14 @@ const allUsers = asyncHandler(async (req, res) => {
       }
     : {};
 
+  // Finding and sending all users except the current user.
   const users = await User.find(keyword)
     .find({ _id: { $ne: req.user._id } })
     .select("-password");
   res.send(users);
 });
 
+// Function to register a new user.
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
 
@@ -25,11 +29,13 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please Enter all the Fields");
   }
+  // Checking if the user already exists.
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
     throw new Error("User already Exists");
   }
+  // Creating a new user and generating a token.
   const user = await User.create({
     name,
     email,
@@ -51,12 +57,16 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Function to authenticate a user.
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
+  // Finding the user by email.
   const user = await User.findOne({ email });
 
+  // Verifying the password.
   if (user && (await user.matchPassword(password))) {
+    // If the password matches, sending user information along with a token.
     res.json({
       _id: user._id,
       name: user.name,
@@ -70,4 +80,5 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Exporting the registerUser, authUser, and allUsers functions to be used in other parts of the application.
 module.exports = { registerUser, authUser, allUsers };
