6a184d72ca81d11fb1375524b72de4a48bac5eb1
Sandeep Kumar Gupta <57314972+sivistinsam@users.noreply.github.com>
2023-08-05 15:41:14 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,30 @@
+// Importing the Express framework.
 const express = require("express");
+
+// Importing the necessary controller functions and middleware.
 const {
   registerUser,
   authUser,
   allUsers,
-} = require("../controllers/userControllers");
-const { protect } = require("../middleware/authMiddleware");
+} = require("../controllers/userControllers"); // Assuming these are your controller functions.
+const { protect } = require("../middleware/authMiddleware"); // Assuming this is your authentication middleware.
 
+// Creating an instance of the Express Router.
 const router = express.Router();
 
+// Defining routes and associating them with controller functions and middleware.
+
+// Route to fetch all users (GET request).
+// Requires authentication using the "protect" middleware.
 router.route("/").get(protect, allUsers);
+
+// Route to register a new user (POST request).
 router.route("/").post(registerUser);
+
+// Route to authenticate a user (POST request).
+// This route is specifically for user login and does not require authentication middleware.
 router.post("/login", authUser);
 
+// Exporting the router to be used in the main application file.
 module.exports = router;
+
