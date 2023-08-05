1ad932b067c1286c3962b88f103b07657006e967
Sandeep Kumar Gupta <57314972+sivistinsam@users.noreply.github.com>
2023-08-05 15:44:08 +0530

    Update ChatProvider.js

--- frontend/src/Context/ChatProvider.js
+++ frontend/src/Context/ChatProvider.js
@@ -1,22 +1,35 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+
+// Creating a context to hold chat-related state.
 const ChatContext = createContext();
 
+// Creating a ChatProvider component to provide the chat-related state to its children.
 const ChatProvider = ({ children }) => {
+  // State variables for user, selected chat, chats, and notifications.
   const [user, setUser] = useState();
   const [selectedChat, setSelectedChat] = useState();
   const [chats, setChats] = useState([]);
   const [notification, setNotification] = useState([]);
 
+  // Accessing the browser's history object using useHistory.
   const history = useHistory();
+
+  // Using useEffect to set the "user" state variable and navigate if user is not logged in.
   useEffect(() => {
+    // Getting user info from local storage.
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+
+    // Setting the "user" state with retrieved user info.
     setUser(userInfo);
 
+    // Redirecting to the login page if user info is missing.
     if (!userInfo) history.push("/");
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [history]);
+
+  // Providing the chat-related state values to the context provider.
   return (
     <ChatContext.Provider
       value={{
@@ -34,8 +47,11 @@ const ChatProvider = ({ children }) => {
     </ChatContext.Provider>
   );
 };
+
+// Creating a ChatState custom hook to access the context values.
 export const ChatState = () => {
   return useContext(ChatContext);
 };
-// useContext(contextValue);
+
+// Exporting the ChatProvider component as the default export.
 export default ChatProvider;
