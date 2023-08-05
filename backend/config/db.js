51e75d4389e105f2348102b015c524379f7a125a
Sandeep Kumar Gupta <57314972+sivistinsam@users.noreply.github.com>
2023-08-05 15:31:52 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,24 @@
+// Importing the Mongoose library for working with MongoDB.
 const mongoose = require("mongoose");
 
+// Defining an asynchronous function named "connectDB" to establish a connection to the MongoDB database.
 const connectDB = async () => {
   try {
+    // Using Mongoose's "connect" method to establish a connection to the MongoDB database.
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useNewUrlParser: true,          // Use the new URL parser.
+      useUnifiedTopology: true,      // Use the new unified topology engine.
     });
+
+    // Logging a success message with the connected host.
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (err) {
+    // If an error occurs while connecting, log the error message and exit the Node.js process.
     console.log(`Error connecting to database: ${err}`.red.bold);
-    process.exit(); //to exit the node app if error occurs while connection with db
+    process.exit(); // Exit the Node.js app if an error occurs during database connection.
   }
 };
+
+// Exporting the "connectDB" function to be used in other parts of the application.
 module.exports = connectDB;
+
