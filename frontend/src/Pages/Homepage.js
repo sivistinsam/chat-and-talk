764142b67917481fc62bb73d956b7664a9ee902c
Sandeep Kumar Gupta <57314972+sivistinsam@users.noreply.github.com>
2023-08-05 15:45:49 +0530

    Update Homepage.js

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -13,35 +13,43 @@ import Login from "../components/Authentication/Login";
 import Signup from "../components/Authentication/Signup";
 import { useHistory } from "react-router-dom";
 
+// Homepage component
 const Homepage = () => {
+  // Accessing the browser's history object using useHistory.
   const history = useHistory();
+
+  // Using useEffect to check if the user is already logged in and redirecting if true.
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("userInfo"));
 
     if (user) history.push("/chats");
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [history]);
+
+  // Rendering the homepage content.
   return (
     <Container maxW="xl" centerContent>
+      {/* Header section */}
       <Box
         display="flex"
         justifyContent="center"
         textAlign={"center"}
         p={3}
         bg="white"
-        // variant="soft-rounded"
         w="100%"
         m="40px 0 15px 0"
         borderRadius="lg"
         borderWidth="1px"
-        // backgroundBlendMode="color-burn"
       >
         <Text fontSize="4xl" fontFamily="Work sans" color="black">
           Chat & Talk
         </Text>
       </Box>
+
+      {/* Tabs for Login and Sign Up */}
       <Box
         bg="white"
-        // backgroundBlendMode="color-burn"
         w="100%"
         p={4}
         borderRadius="lg"
@@ -55,9 +63,11 @@ const Homepage = () => {
           </TabList>
           <TabPanels>
             <TabPanel>
+              {/* Rendering the Login component */}
               <Login />
             </TabPanel>
             <TabPanel>
+              {/* Rendering the Signup component */}
               <Signup />
             </TabPanel>
           </TabPanels>
