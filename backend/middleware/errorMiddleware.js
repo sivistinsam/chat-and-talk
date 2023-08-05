c36b899cee88fb9a55c2f2da8a75d0e6c4761aa4
Sandeep Kumar Gupta <57314972+sivistinsam@users.noreply.github.com>
2023-08-05 15:38:50 +0530

    Update errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,16 +1,26 @@
+// Middleware to handle "Not Found" errors.
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
+  res.status(404); // Setting the response status code to 404 (Not Found).
+  next(error); // Passing the error to the next middleware.
 };
 
+// Middleware to handle errors during request processing.
 const errorHandler = (err, req, res, next) => {
+  // Determining the status code to use based on the current response status.
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  // Setting the response status code to the determined status code.
   res.status(statusCode);
+
+  // Sending back an error response containing the error message.
+  // If in production environment, stack trace is not included for security.
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
 
+// Exporting the "notFound" and "errorHandler" middleware functions.
 module.exports = { notFound, errorHandler };
+
