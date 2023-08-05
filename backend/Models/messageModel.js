0323a028f46409399e1e039e1cb22d479c0440cb
Sandeep Kumar Gupta <57314972+sivistinsam@users.noreply.github.com>
2023-08-05 15:29:29 +0530

    Update messageModel.js

--- backend/Models/messageModel.js
+++ backend/Models/messageModel.js
@@ -1,17 +1,29 @@
+// Importing the Mongoose library for working with MongoDB.
 const mongoose = require("mongoose");
 
+// Defining a Mongoose Schema for the 'Message' model.
 const messageModel = mongoose.Schema(
   {
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    // Defining a field for the reference to the user who sent the message.
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Refers to the "User" model.
+
+    // Defining a field for the content of the message.
     content: { type: String, trim: true },
-    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
-    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+
+    // Defining a field for the reference to the chat in which the message was sent.
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" }, // Refers to the "Chat" model.
+
+    // Defining a field for an array of user references who have read the message.
+    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Refers to the "User" model.
   },
   {
+    // Adding timestamps to the document to automatically track creation and update times.
     timestamps: true,
   }
 );
 
+// Creating the Mongoose model "Message" based on the defined schema.
 const Message = mongoose.model("Message", messageModel);
 
+// Exporting the "Message" model to be used in other parts of the application.
 module.exports = Message;
